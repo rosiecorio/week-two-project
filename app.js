2a3dcc9c1cbd76e4fad2204a875549878a812a8d
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-15 20:15:40 +0000

    added html and css for preview bar and footer

---
+++ app.js
@@ -0,0 +1,7 @@
+const forest = document.getElementById('forest')
+const leaves = document.getElementById('leaves')
+const sunlight = document.getElementById('sunlight')
+const bark = document.getElementById('bark')
+const sapling = document.getElementById('sapling')
+
+const imageArray = [forest, leaves, sunlight, bark, sapling]
